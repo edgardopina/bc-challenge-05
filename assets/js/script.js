80a053611cb3104355c93d699d3eb2908ce772eb
edgardopina <edgardopina@yahoo.com>
2021-08-27 19:34:58 -0600

    Open planner and display current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-//
+var startPlanner = function () {
+
+};
+
+var updateCurrentDay = function () {
+	var currentDate = document.querySelector("#currentDay");
+	currentDate.innerHTML = moment().format("dddd, MMMM Do");
+};
+
+var headerIntervalCtrl = setInterval(updateCurrentDay, 1000);
+
+startPlanner();
