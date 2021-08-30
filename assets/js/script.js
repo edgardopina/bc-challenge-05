8fcf453d6866fd49318e64be720de1127e015b31
edgardopina <edgardopina@yahoo.com>
2021-08-29 21:30:03 -0600

    Completed enter/update event, save event, refresh from localStorage, styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,52 @@
 const START_OF_DAY_TIME = 9;
 const END_OF_DAY_TIME = 18;
-var tempArray = [];
-var tasks = [];
 const LOC_STG_APP_KEY = "scheduler";
+const TIME_TICK = 1000;
+var tasks = {};
 var dayGroup;
-var taskObj = {
-	timeBlock: 0,
-	taskEl: "",
-};
+var flagLocStg = true;
 
 var saveTasks = function () {
 	localStorage.setItem(LOC_STG_APP_KEY, JSON.stringify(tasks));
 };
 
+function setTaskStatusColor(hour) {
+	if (hour > moment().hour(11).format("H")) {
+		return "col-8 description future";
+	} else if (hour == moment().hour(11).format("H")) {
+		return "col-8 description present";
+	} else {
+		return "col-8 description past";
+	}
+}
+
 var createDay = function () {
-	tasks = JSON.parse(localStorage.getItem(LOC_STG_APP_KEY));
 	for (var hour = START_OF_DAY_TIME; hour < END_OF_DAY_TIME; ++hour) {
 		var liElement = document.createElement("li");
 		liElement.className = "row day-item";
 
 		var timeBlock = document.createElement("span");
-		timeBlock.className = "col";
-		timeBlock.innerHTML = moment(hour, "HH").format("h : mm a");
+		timeBlock.className = "col-2 hour time-block";
+		timeBlock.textContent = moment(hour, "HH").format("h a");
 
 		var taskElement = document.createElement("p");
-
-		var saveIcon = document.createElement("span");
-		saveIcon.className = "col oi oi-task";
-
-		if (!tasks) {
-			taskElement.innerHTML = "dummy task " + hour;
+		taskElement.className = setTaskStatusColor(hour);
+		var tempDescr = {};
+		if (flagLocStg) {
+			tempDescr = tasks[hour - START_OF_DAY_TIME];
 		} else {
-			taskElement.innerHTML = tasks[hour - START_OF_DAY_TIME].taskDescription;
+			tempDescr = "";
 		}
+		taskElement.textContent = tempDescr;
 
-		if (hour > moment().hour(11).format("H")) {
-			taskElement.className = "col future";
-		} else if (hour == moment().hour(11).format("H")) {
-			taskElement.className = "col present";
-		} else {
-			taskElement.className = "col past";
-		} // "this" is the current element in the loop
+		var saveIcon = document.createElement("i");
+		saveIcon.className = "col-2 oi oi-task saveBtn";
 
 		liElement.appendChild(timeBlock);
 		liElement.appendChild(taskElement);
 		liElement.appendChild(saveIcon);
 		dayGroup.appendChild(liElement);
-
-		taskObj.timeBlock = hour;
-		taskObj.taskEl = taskElement;
-		tempArray.push(taskObj);
 	}
-	tasks = tempArray;
-	saveTasks();
 };
 
 var createUl = function () {
@@ -64,13 +58,17 @@ var createUl = function () {
 var updateCurrentDay = function () {
 	var currentDate = document.querySelector("#currentDay");
 	currentDate.innerHTML = moment().format("dddd, MMMM Do");
-	if (moment().minute() == "00") {
-		for (var hour = START_OF_DAY_TIME; hour < END_OF_DAY_TIME; ++hour) {}
-	}
 };
 
 var startScheduler = function () {
-	var headerIntervalCtrl = setInterval(updateCurrentDay, 1000);
+	var headerIntervalCtrl = setInterval(updateCurrentDay, TIME_TICK);
+	tasks = JSON.parse(localStorage.getItem("scheduler"));
+	console.log(tasks);
+	if (!tasks) {
+		tasks = [];
+		flagLocStg = false;
+	}
+	console.log(tasks);
 	createUl();
 	createDay();
 };
@@ -78,39 +76,23 @@ var startScheduler = function () {
 startScheduler();
 
 $(".day-group").on("click", "p", function () {
-	// gets task description into var text
-	var text = $(this).text().trim();
-	// console.log("in listener: ", text);
-
-	// creates <textarea> element, adds class name, and value
-	var textInput = $("<textarea>").addClass("form-control").val(text);
-
-	// Swap old text description with updated description; <textarea>
-	$(this).replaceWith(textInput);
-
-	// programatically "triggers" the event "focus" on textInput to avoid the need
-	// to click on <textarea> to start the text descr update
+	var text = $(this).text().trim(); // gets task description into var text
+	var textInput = $("<textarea>").addClass("col-8").val(text); // creates <textarea> element, adds class name, and value
+	$(this).replaceWith(textInput); // Swap old text description with updated description; <textarea>
 	textInput.trigger("focus");
 });
 
 $(".day-group").on("blur", "textarea", function () {
-	// get the textarea's current value/text
-	var text = $(this).val().trim();
-
-	// get the parent ul's id attribute by transversing-up to the closest
-	// var status = $(this).closest(".day-group").attr("id").replace("list-", "");
-	var index = $(this).closest(".day-item").index();
-
-	tasks[index].taskEl.textContent = text;
-
-	// persists updated task description
-	// saveTasks(); ******************************************
-
-	// recreate <p> element with updated text
-	var taskP = $("<p>").addClass("col").text(text);
-
-	// replace textarea with <p> element
-	$(this).replaceWith(taskP);
+	var text = $(this).val().trim(); // get the textarea's current value/text
+	var index = $(this).closest(".day-item").index(); // transversing-up to the closest (itself)
+	tasks[index] = text; // updates tasks array
+	// recreate <p> element with updated text and update status color
+	var taskP = $("<p>")
+		.addClass(setTaskStatusColor(index + START_OF_DAY_TIME))
+		.text(text);
+	$(this).replaceWith(taskP); // replace textarea with <p> element
 });
 
-// console.log(document.querySelector(".day-group"));
+$(".day-group").on("click", "i", function () {
+	localStorage.setItem("scheduler", JSON.stringify(tasks));
+});
